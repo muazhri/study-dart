76b08c435da3ea0777c7bc41d8ba1967ba60020f
M. Azis Muhemin Tohari <muhaiminaziz216@gmail.com>
2023-05-17 15:29:08 +0700

    tambah file string.dart

--- hello.dart
+++ hello.dart
@@ -1,16 +1,27 @@
+// Belajar tipe data dan Operator
+
+// // variable, print, dan tipe data
 void main(a, b) {
+  // string
   var name = 'Voyager I';
+  // integer
   var year = 1977;
+  // double
   var antennaDiameter = 3.7;
+  // list
   var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  // map
   var image = {
     'tags': ['saturn'],
     'url': '//path/to/saturn.jpg'
   };
+  // boolean
+  var hasLife = true;
 
-  print(name);
+  print('Nama : ${name}');
   print(year);
   print(antennaDiameter);
-  print(flybyObjects);
+  print('Nama planet : ${flybyObjects[1]}');
   print(image);
+  print(hasLife);
 }
